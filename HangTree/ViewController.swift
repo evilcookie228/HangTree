ece006d9a8f40f153c52ca0b6159a9de52c6149c
Programming <programming@macbook-pro-programming-2.local>
2019-02-02 16:14:30 +0300

    Initial Commit

---
+++ HangTree/ViewController.swift
@@ -0,0 +1,33 @@
+//
+//  ViewController.swift
+//  HangTree
+//
+//  Created by Programming on 02.02.2019.
+//  Copyright © 2019 Programming. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    @IBOutlet weak var topStackView: UIStackView!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+    
+    override func viewWillTransition(to size: CGSize, with coordinator: UIViewControllerTransitionCoordinator)
+        {
+            super.viewWillTransition(to: size, with: coordinator)
+            if size.width < size.height
+            {
+                topStackView.axis = .vertical
+            }
+            else
+            {
+                topStackView.axis = .horizontal
+            }
+    }
+}
+
