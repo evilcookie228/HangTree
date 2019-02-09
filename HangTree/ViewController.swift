dd70c614309dd0f6b3f4eb7b0b39984e28391810
Programming <programming@macbook-pro-programming-2.local>
2019-02-09 16:23:37 +0300

    jhgvbnhgfv\

--- HangTree/ViewController.swift
+++ HangTree/ViewController.swift
@@ -11,6 +11,9 @@ import UIKit
 class ViewController: UIViewController {
 
     @IBOutlet weak var topStackView: UIStackView!
+    @IBOutlet weak var treeImageView: UIImageView!
+    @IBOutlet weak var correctWordLabel: UILabel!
+    @IBOutlet weak var scoreLabel: UILabel!
     
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -28,6 +31,12 @@ class ViewController: UIViewController {
             {
                 topStackView.axis = .horizontal
             }
+    }
+    
+    @IBAction func buttonPressed(_ sender: UIButton)
+    {
+        print(sender.currentTitle ?? "nil")
+        sender.isEnabled = false
     }
 }
 
